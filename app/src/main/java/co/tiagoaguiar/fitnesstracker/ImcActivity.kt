b9a46e1836a707f56e15a2d17072548dc5808684
Matheus Nicacio <110930694+matheussnicacio@users.noreply.github.com>
2025-03-10 12:10:12 -0300

    Validacao de formularios

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,12 +1,20 @@
 package co.tiagoaguiar.fitnesstracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -15,10 +23,70 @@ class ImcActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_imc)
 
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_weight)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+
+            if (!validate()) {
+
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+
+                return@setOnClickListener
+
+            }
+            // Aqui deu sucesso
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
+
+    private fun validate(): Boolean {
+        // nao pode inserir valores nulos / vazio
+        // nao pode inserir / comecao com 0
+
+        // true && true = true
+        // true && false = false
+        // false && true = false
+        // false && false = false
+
+        // opçao 1 : Usar if else
+//        if (editWeight.text.toString().isNotEmpty()
+//            && editHeight.text.toString().isNotEmpty()
+//            && !editWeight.text.toString().startsWith("0")
+//            && !editHeight.text.toString().startsWith("0")
+//        ) {
+//            return true
+//        } else {
+//            return false
+//        }
+
+        // opção 2 : Usar somente return pra simular if e else
+//        if (editWeight.text.toString().isNotEmpty()
+//            && editHeight.text.toString().isNotEmpty()
+//            && !editWeight.text.toString().startsWith("0")
+//            && !editHeight.text.toString().startsWith("0")
+//        ) {
+//            return true
+//        }
+//        return false
+//    }
+
+
+        // opcao 3 : Forma mais  enxuta possivel para retornar um true | false
+        // return direto o que for verdadeiro
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && !editWeight.text.toString().startsWith("0")
+                && !editHeight.text.toString().startsWith("0")
+                )
+    }
+
 }
